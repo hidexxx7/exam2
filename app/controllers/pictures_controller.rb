88ca6f5b11fa46f2dcb9eb805fee79769ea4b0f2
小島秀樹 <kojimahideki@MacBook-Pro.local>
2017-08-11 23:34:16 +0900

    1st commit

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,43 @@
+class PicturesController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @pictures = Picture.all
+  end
+
+  def new
+    @picture = Picture.new
+  end
+
+  def create
+    @picture = Picture.create(pictures_params)
+    @picture.user_id = current_user.id
+    if @picture.save
+      redirect_to pictures_path, notice: "写真を投稿しました！"
+    else
+      render 'new'
+    end
+
+  end
+
+  def edit
+    @picture = Picture.find(params[:id])
+  end
+
+  def update
+    @picture = Picture.find(params[:id])
+    @picture.update(pictures_params)
+    redirect_to pictures_path, notice: "写真を更新しました！"
+  end
+
+  def destroy
+    @picture = Picture.find(params[:id])
+    @picture.destroy
+    redirect_to pictures_path, notice: "写真を削除しました！"
+  end
+
+  private
+    def pictures_params
+      params.require(:picture).permit(:title, :content)
+    end
+end
